f1669db101eb983dc8f685e19d6ca50fe65499d4
Bulava Eduard <bulavaeduard@gmail.com>
2016-11-25 18:51:43 +0200

    added GET and POST

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,9 +5,12 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController extends Controller
+class DefaultController extends Controller
 {
     /**
      * @Route("/fill", name="fill")
@@ -18,12 +21,25 @@ class ArticleController extends Controller
     }
 
     /**
-     * @Route("/article/new", name="new_article")
+     * @Route("/new", name="new")
      * @Method("POST")
      */
     public function newAction(Request $request)
     {
 
+        $entity = new \stdClass();
+        $entity->title = 'Какой-то заголовок'.rand();
+        $entity->content = 'Какой-то контент'.rand();
+
+        $fs = new Filesystem();
+
+        $fs->dumpFile(__DIR__.'/../../../web/db/newFile'.rand(0,10).'.json', json_encode($entity));
+
+        $response = new JsonResponse($entity, 201);
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 
     /**
@@ -32,7 +48,22 @@ class ArticleController extends Controller
      */
     public function listAction()
     {
+        $handle = opendir(__DIR__.'/../../../web/db');
 
+        $files = [];
+
+        while (false !== ($file = readdir($handle))) {
+
+            if ($file !== '.' && $file !== '..'){
+                $files[] = json_decode(file_get_contents(__DIR__.'/../../../web/db/'.$file));
+            }
+
+        }
+        $response = new JsonResponse($files, 200);
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 
     /**
